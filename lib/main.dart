835e1f52df6d04553251557ddf94dbee1d24a178
Najmuddin <njmdnyusof.work@gmail.com>
2024-10-22 00:05:56 +0800

    common

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:henshin/splash/splash_widget.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool displaySplashImage = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        Duration(seconds: 1), () => setState(() => displaySplashImage = false));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Henshin-App',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en', '')],
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: displaySplashImage
+          ? Container(
+              color: Colors.white,
+              child: Builder(
+                builder: (context) => Image.asset(
+                  'assets/images/shot-cropped-1576957517751.png',
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+            )
+          : SplashWidget(),
     );
   }
 }
