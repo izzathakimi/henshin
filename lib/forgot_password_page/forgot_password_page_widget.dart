255966372d47310196dd30045ac307681081fef9
Najmuddin <njmdnyusof.work@gmail.com>
2024-10-22 00:56:26 +0800

    batu

---
+++ lib/forgot_password_page/forgot_password_page_widget.dart
@@ -0,0 +1,159 @@
+import '../common/Henshin_theme.dart';
+import '../common/Henshin_widgets.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPasswordPageWidget extends StatefulWidget {
+  const ForgotPasswordPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordPageWidgetState createState() =>
+      _ForgotPasswordPageWidgetState();
+}
+
+class _ForgotPasswordPageWidgetState extends State<ForgotPasswordPageWidget> {
+  TextEditingController? textController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        leading: InkWell(
+          onTap: () async {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.keyboard_arrow_left_outlined,
+            color: Colors.black,
+            size: 24,
+          ),
+        ),
+        actions: [],
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                child: Text(
+                  'Forgot Password',
+                  style: HenshinTheme.bodyText1.override(
+                    fontFamily: 'NatoSansKhmer',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    useGoogleFonts: false,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 32, 16, 0),
+                  child: Text(
+                    'Enter your e-mail address bellow to receive the code for setting up a new password.',
+                    textAlign: TextAlign.center,
+                    style: HenshinTheme.bodyText1.override(
+                      fontFamily: 'NatoSansKhmer',
+                      color: Color(0xCB303030),
+                      useGoogleFonts: false,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 32, 0, 0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(32, 0, 32, 0),
+                    child: TextFormField(
+                      controller: textController,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        hintText: 'Email Address',
+                        hintStyle: HenshinTheme.bodyText1.override(
+                          fontFamily: 'NatoSansKhmer',
+                          color: Color(0xB3303030),
+                          useGoogleFonts: false,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x98757575),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x98757575),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                      ),
+                      style: HenshinTheme.bodyText1,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(32, 45, 32, 0),
+                  child: FFButtonWidget(
+                    onPressed: () {
+                      print('Button pressed ...');
+                    },
+                    text: 'Send Code',
+                    options: FFButtonOptions(
+                      width: 130,
+                      height: 45,
+                      color: HenshinTheme.primaryColor,
+                      textStyle: HenshinTheme.subtitle2.override(
+                        fontFamily: 'NatoSansKhmer',
+                        color: Colors.white,
+                        useGoogleFonts: false,
+                      ),
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1,
+                      ),
+                      borderRadius: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/verify_account_page/verify_account_widget.dart
@@ -0,0 +1,187 @@
+import '../common/henshin_theme.dart';
+import '../common/henshin_widgets.dart';
+import 'package:flutter/material.dart';
+
+class VerifyAccountPageWidget extends StatefulWidget {
+  const VerifyAccountPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _VerifyAccountPageWidgetState createState() =>
+      _VerifyAccountPageWidgetState();
+}
+
+class _VerifyAccountPageWidgetState extends State<VerifyAccountPageWidget> {
+  TextEditingController? textController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        leading: InkWell(
+          onTap: () async {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.keyboard_arrow_left_outlined,
+            color: Colors.black,
+            size: 24,
+          ),
+        ),
+        actions: [],
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                child: Text(
+                  'Verify Account',
+                  style: HenshinTheme.bodyText1.override(
+                    fontFamily: 'NatoSansKhmer',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    useGoogleFonts: false,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 32, 16, 0),
+                  child: Text(
+                    'Enter the code or click on the link received via e-mail, to confirm your account.',
+                    textAlign: TextAlign.center,
+                    style: HenshinTheme.bodyText1.override(
+                      fontFamily: 'NatoSansKhmer',
+                      color: Color(0xCB303030),
+                      useGoogleFonts: false,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 32, 0, 0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(32, 0, 32, 0),
+                    child: TextFormField(
+                      controller: textController,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        hintText: 'Enter OPT code',
+                        hintStyle: HenshinTheme.bodyText1.override(
+                          fontFamily: 'NatoSansKhmer',
+                          color: Color(0xB3303030),
+                          useGoogleFonts: false,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x98757575),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x98757575),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                      ),
+                      style: HenshinTheme.bodyText1,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(16, 45, 16, 0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                  child: Text(
+                    'Didn\'t recieve?',
+                    style: HenshinTheme.bodyText1,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                  child: Text(
+                    'Resend code',
+                    style: HenshinTheme.bodyText1.override(
+                      fontFamily: 'NatoSansKhmer',
+                      color: HenshinTheme.primaryColor,
+                      fontWeight: FontWeight.bold,
+                      useGoogleFonts: false,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(32, 45, 32, 0),
+                  child: FFButtonWidget(
+                    onPressed: () {
+                      print('Button pressed ...');
+                    },
+                    text: 'Verify Code',
+                    options: FFButtonOptions(
+                      width: 130,
+                      height: 45,
+                      color: HenshinTheme.primaryColor,
+                      textStyle: HenshinTheme.subtitle2.override(
+                        fontFamily: 'NatoSansKhmer',
+                        color: Colors.white,
+                        useGoogleFonts: false,
+                      ),
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1,
+                      ),
+                      borderRadius: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
